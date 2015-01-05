a8d78b9d106f31f83675c100c23ed9f9d45d41b0
Ivan <ivanmondragonguerrero@gmail.com>
2015-01-05 00:04:16 -0600

    cambios en el menu
    agregue el menu, yo creo que hay que quitarlo mas a delante

--- administrador/application/controllers/admin.php
+++ administrador/application/controllers/admin.php
@@ -12,6 +12,7 @@ function __construct()
         /* ------------------ */    
  
         $this->load->library('grocery_CRUD');
+        $this->load->library('image_CRUD');  
 	}
 
 	function _example_output($output = null)
@@ -24,12 +25,31 @@ function index(){
 	$crud = new grocery_CRUD();
         $crud->set_table('casas');
         $crud->set_subject('Casa');
+        $crud->columns('precio','terreno_m2','construccion_m2');
         $crud->set_language("spanish");
+        $crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
         $output = $crud->render();
         $this->_example_output($output);
 }
 
+function imageUpload($primary_key)
+{
+      
+    
+   
+    $image_crud = new image_CRUD();
+ 
+    $image_crud->set_table('example_1');
+    
+    //If your table have by default the "id" field name as a primary key this line is not required
+    $image_crud->set_primary_key_field('id');
+    $image_crud->set_url_field('url');
+    $image_crud->set_image_path('assets/uploads');
+    $image_crud->set_relation_field('idCasa');
+    $output = $image_crud->render();
+ 
+    $this->_example_output($output);
 }
 
-
+}
 ?>
--- administrador/application/views/example.php
+++ administrador/application/views/example.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" class="no-js">
 <head>
 	<meta charset="utf-8" />
+
+		<!--<link rel="shortcut icon" href="../favicon.ico">-->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url() . 'assets/css/normalize.css'; ?> />
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url() . 'assets/css/demo.css'; ?> />
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url() . 'assets/css/component.css'; ?> />
+		
+
 <?php 
 foreach($css_files as $file): ?>
 	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
 <?php endforeach; ?>
+<script src=<?php echo base_url() . 'assets/js/modernizr.custom.js'; ?>></script>
 <?php foreach($js_files as $file): ?>
 	<script src="<?php echo $file; ?>"></script>
 <?php endforeach; ?>
@@ -25,22 +33,80 @@ a:hover
 	text-decoration: underline;
 }
 </style>
+		
+		
+
 </head>
 <body>
-	<div>
-		<a href='<?php echo site_url('examples/customers_management')?>'>Customers</a> |
-		<a href='<?php echo site_url('examples/orders_management')?>'>Orders</a> |
-		<a href='<?php echo site_url('examples/products_management')?>'>Products</a> |
-		<a href='<?php echo site_url('examples/offices_management')?>'>Offices</a> | 
-		<a href='<?php echo site_url('examples/employees_management')?>'>Employees</a> |		 
-		<a href='<?php echo site_url('examples/film_management')?>'>Films</a> | 
-		<a href='<?php echo site_url('examples/film_management_twitter_bootstrap')?>'>Twitter Bootstrap Theme [BETA]</a> | 
-		<a href='<?php echo site_url('examples/multigrids')?>'>Multigrid [BETA]</a>
+
+
+	<div class="container">
+			<ul id="gn-menu" class="gn-menu-main">
+				<li class="gn-trigger">
+					<a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+					<nav class="gn-menu-wrapper">
+						<div class="gn-scroller">
+							<ul class="gn-menu">
+								<li class="gn-search-item">
+									<input placeholder="Search" type="search" class="gn-search">
+									<a class="gn-icon gn-icon-search"><span>Search</span></a>
+								</li>
+								<li>
+									<a class="gn-icon gn-icon-download">Downloads</a>
+									<ul class="gn-submenu">
+										<li><a class="gn-icon gn-icon-illustrator">Vector Illustrations</a></li>
+										<li><a class="gn-icon gn-icon-photoshop">Photoshop files</a></li>
+									</ul>
+								</li>
+								<li><a class="gn-icon gn-icon-cog">Settings</a></li>
+								<li><a class="gn-icon gn-icon-help">Help</a></li>
+								<li>
+									<a class="gn-icon gn-icon-archive">Archives</a>
+									<ul class="gn-submenu">
+										<li><a class="gn-icon gn-icon-article">Articles</a></li>
+										<li><a class="gn-icon gn-icon-pictures">Images</a></li>
+										<li><a class="gn-icon gn-icon-videos">Videos</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div><!-- /gn-scroller -->
+					</nav>
+				</li>
+				<li><a href="http://tympanus.net/codrops">Codrops</a></li>
+				<li><a class="codrops-icon codrops-icon-prev" href="http://tympanus.net/Development/HeaderEffects/"><span>Previous Demo</span></a></li>
+				<li><a class="codrops-icon codrops-icon-drop" href="http://tympanus.net/codrops/?p=16030"><span>Back to the Codrops Article</span></a></li>
+			</ul>
+<!--
+			<div >
+				<?php echo $output; ?>
+    		</div>
+    		-->
+			
+			<header>
+				<?php echo $output; ?>
+			</header> 
 		
+		</div><!-- /container -->
+		<script src=<?php echo base_url() . 'assets/js/classie.js'; ?>></script>
+		<script src=<?php echo base_url() . 'assets/js/gnmenu.js'; ?>></script>
+		<script>
+			new gnMenu( document.getElementById( 'gn-menu' ) );
+		</script>
+
+
+<!--
+	<div>
+		<a href='<?php echo site_url('images_examples/example1')?>'>Example 1 - Simple</a> |
+		<a href='<?php echo site_url('images_examples/example2')?>'>Example 2 - Ordering</a> |
+		<a href='<?php echo site_url('images_examples/example3/22')?>'>Example 3 - With group id</a> |
+		<a href='<?php echo site_url('images_examples/example4')?>'>Example 4 - Images with title</a> | 
+		<a href='<?php echo site_url('images_examples/simple_photo_gallery')?>'>Simple Photo Gallery</a>
 	</div>
 	<div style='height:20px;'></div>  
-    <div>
-		<?php echo $output; ?>
-    </div>
+-->
+   
+
+
+
 </body>
 </html>
