d3f3b793c987400cd6bc359ef1b22dc65ee395a4
edguitarhero <emondragonguerrero@gmail.com>
2015-01-10 00:52:16 -0600

    arreglar image crud

--- administrador/application/controllers/admin.php
+++ administrador/application/controllers/admin.php
@@ -19,37 +19,321 @@ function __construct()
     {
         $this->load->view('example.php',$output);  
 
-    }
+}
 
 function index(){
+    //echo "hola".$this->getRandomCode();
+    if($this->session->userdata('logged_in'))
+   {
 	$crud = new grocery_CRUD();
         $crud->set_table('casas');
         $crud->set_subject('Casa');
-        $crud->columns('precio','terreno_m2','construccion_m2');
+        $crud->columns('precio','terreno_m2','direccion_colonia','direccion_calle','direccion_numero');
+        //$crud->fields('precio','rama','fecha_inicio','fecha_termino', 'puntos_por_ganar', 'puntos_por_perder');
+        $crud->required_fields('venta_renta','terreno_m2','construccion_m2','direccion_numero','direccion_calle','direccion_colonia',
+            'direccion_municipio','direccion_estado','recamaras','banos','precio',
+            'agua','drenaje','luz','contacto_telefono','descripcion');
+        $crud->display_as('banos','Baños');
+        $crud->display_as('venta_renta','Venta o Renta');
+        $crud->display_as('construccion_m2','Construccion en metros cuadrados');
+        $crud->display_as('terreno_m2','Terreno en metros cuadrados');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_numero','Numero');
+        $crud->display_as('direccion_colonia','Colonia');
+        $crud->display_as('direccion_municipio','Municipio');
+        $crud->display_as('direccion_estado','Estado');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_codigo_postal','Codigo Postal');
+        $crud->display_as('direccion_pais','Pais');
+        $crud->display_as('direccion_latitud','Latitud');
+        $crud->display_as('direccion_longitud','Longitud');
+        $crud->display_as('espacio_autos','Espacio para Autos');
+        $crud->display_as('contacto_telefono','Telefono');
+        $crud->display_as('contacto_email','Email');
+        //$crud->field_type('primer_rama','enum',array('femenil','varonil'));
+        $crud->field_type('idcasa', 'hidden', $this->getRandomCode());
         $crud->set_language("spanish");
         $crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
         $output = $crud->render();
         $this->_example_output($output);
 }
+else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+}
+
+function terrenos(){
+    if($this->session->userdata('logged_in'))
+   {
+    $crud = new grocery_CRUD();
+        $crud->set_table('terrenos');
+        $crud->set_subject('Terreno');
+        $crud->columns('precio','terreno_m2','direccion_colonia','direccion_calle','direccion_numero');
+        $crud->required_fields('venta_renta','terreno_m2','direccion_numero','direccion_calle','direccion_colonia',
+            'direccion_municipio','direccion_estado','precio',
+            'agua','drenaje','luz','contacto_telefono','descripcion');
+        $crud->display_as('venta_renta','Venta o Renta');
+        $crud->display_as('terreno_m2','Terreno en metros cuadrados');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_numero','Numero');
+        $crud->display_as('direccion_colonia','Colonia');
+        $crud->display_as('direccion_municipio','Municipio');
+        $crud->display_as('direccion_estado','Estado');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_codigo_postal','Codigo Postal');
+        $crud->display_as('direccion_pais','Pais');
+        $crud->display_as('direccion_latitud','Latitud');
+        $crud->display_as('direccion_longitud','Longitud');
+        $crud->display_as('contacto_telefono','Telefono');
+        $crud->display_as('contacto_email','Email');
+        $crud->field_type('idterrenos', 'hidden', $this->getRandomCode());
+        $crud->set_language("spanish");
+        //$crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
+        $output = $crud->render();
+        $this->_example_output($output);
+}
+else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+}
+
+function departamentos(){
+    if($this->session->userdata('logged_in'))
+   {
+    $crud = new grocery_CRUD();
+        $crud->set_table('departamentos');
+        $crud->set_subject('Departamento');
+        $crud->columns('precio','terreno_m2','direccion_colonia','direccion_calle','direccion_numero');
+        $crud->required_fields('venta_renta','terreno_m2','construccion_m2','direccion_numero','direccion_calle','direccion_colonia',
+            'direccion_municipio','direccion_estado','recamaras','banos','precio',
+            'agua','drenaje','luz','contacto_telefono','descripcion');
+        $crud->display_as('banos','Baños');
+        $crud->display_as('venta_renta','Venta o Renta');
+        $crud->display_as('construccion_m2','Construccion en metros cuadrados');
+        $crud->display_as('terreno_m2','Terreno en metros cuadrados');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_numero','Numero');
+        $crud->display_as('direccion_colonia','Colonia');
+        $crud->display_as('direccion_municipio','Municipio');
+        $crud->display_as('direccion_estado','Estado');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_codigo_postal','Codigo Postal');
+        $crud->display_as('direccion_pais','Pais');
+        $crud->display_as('direccion_latitud','Latitud');
+        $crud->display_as('direccion_longitud','Longitud');
+        $crud->display_as('espacio_autos','Espacio para Autos');
+        $crud->display_as('contacto_telefono','Telefono');
+        $crud->display_as('contacto_email','Email');
+        $crud->field_type('iddepartamentos', 'hidden', $this->getRandomCode());
+        $crud->set_language("spanish");
+        $crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
+        $output = $crud->render();
+        $this->_example_output($output);
+}
+else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+}
+
+function bodegas(){
+    if($this->session->userdata('logged_in'))
+   {
+    $crud = new grocery_CRUD();
+        $crud->set_table('bodegas');
+        $crud->set_subject('Bodega');
+        $crud->columns('precio','terreno_m2','direccion_colonia','direccion_calle','direccion_numero');
+        $crud->required_fields('venta_renta','terreno_m2','construccion_m2','direccion_numero','direccion_calle','direccion_colonia',
+            'direccion_municipio','direccion_estado','recamaras','banos','precio',
+            'agua','drenaje','luz','contacto_telefono','descripcion');
+        $crud->display_as('banos','Baños');
+        $crud->display_as('venta_renta','Venta o Renta');
+        $crud->display_as('construccion_m2','Construccion en metros cuadrados');
+        $crud->display_as('terreno_m2','Terreno en metros cuadrados');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_numero','Numero');
+        $crud->display_as('direccion_colonia','Colonia');
+        $crud->display_as('direccion_municipio','Municipio');
+        $crud->display_as('direccion_estado','Estado');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_codigo_postal','Codigo Postal');
+        $crud->display_as('direccion_pais','Pais');
+        $crud->display_as('direccion_latitud','Latitud');
+        $crud->display_as('direccion_longitud','Longitud');
+        $crud->display_as('espacio_autos','Espacio para Autos');
+        $crud->display_as('contacto_telefono','Telefono');
+        $crud->display_as('contacto_email','Email');
+        $crud->display_as('ideal','Ideal para');
+        $crud->display_as('patio_maniobras','Patio de Maniobras');
+        $crud->display_as('montacarga','Zona de Montacargas');
+        $crud->field_type('idbodega', 'hidden', $this->getRandomCode());
+        $crud->set_language("spanish");
+        //$crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
+        $output = $crud->render();
+        $this->_example_output($output);
+}
+else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+}
+
+function oficinas(){
+    if($this->session->userdata('logged_in'))
+   {
+    $crud = new grocery_CRUD();
+        $crud->set_table('oficinas');
+        $crud->set_subject('Oficina');
+        $crud->columns('precio','terreno_m2','direccion_colonia','direccion_calle','direccion_numero');
+        $crud->required_fields('venta_renta','terreno_m2','construccion_m2','direccion_numero','direccion_calle','direccion_colonia',
+            'direccion_municipio','direccion_estado','recamaras','banos','precio',
+            'agua','drenaje','luz','contacto_telefono','descripcion');
+        $crud->display_as('banos','Baños');
+        $crud->display_as('venta_renta','Venta o Renta');
+        $crud->display_as('construccion_m2','Construccion en metros cuadrados');
+        $crud->display_as('terreno_m2','Terreno en metros cuadrados');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_numero','Numero');
+        $crud->display_as('direccion_colonia','Colonia');
+        $crud->display_as('direccion_municipio','Municipio');
+        $crud->display_as('direccion_estado','Estado');
+        $crud->display_as('direccion_calle','Calle');
+        $crud->display_as('direccion_codigo_postal','Codigo Postal');
+        $crud->display_as('direccion_pais','Pais');
+        $crud->display_as('direccion_latitud','Latitud');
+        $crud->display_as('direccion_longitud','Longitud');
+        $crud->display_as('espacio_autos','Espacio para Autos');
+        $crud->display_as('contacto_telefono','Telefono');
+        $crud->display_as('contacto_email','Email');
+        $crud->display_as('sala_juntas','Sala de Juntas');
+        $crud->display_as('numero_usuarios','Numero de Usuarios');
+        $crud->field_type('idoficina', 'hidden', $this->getRandomCode());
+        $crud->set_language("spanish");
+        //$crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
+        $output = $crud->render();
+        $this->_example_output($output);
+}
+else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+}
+
+function locales(){
+     if($this->session->userdata('logged_in'))
+   {
+    $crud = new grocery_CRUD();
+        $crud->set_table('locales');
+        $crud->set_subject('Local');
+        $crud->required_fields('venta_renta','terreno_m2','construccion_m2','direccion_numero','direccion_calle','direccion_colonia',
+            'direccion_municipio','direccion_estado','recamaras','banos','precio',
+            'agua','drenaje','luz','contacto_telefono','descripcion');
+        $crud->columns('precio','terreno_m2','direccion_colonia','direccion_calle','direccion_numero');
+        $crud->field_type('idlocal', 'hidden', $this->getRandomCode());
+        $crud->set_language("spanish");
+        $crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
+        $output = $crud->render();
+        $this->_example_output($output);
+}
+else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+}
+
+function nave_industrial(){
+     if($this->session->userdata('logged_in'))
+   {
+    $crud = new grocery_CRUD();
+        $crud->set_table('nave_industrial');
+        $crud->set_subject('Naves Industriales');
+        $crud->columns('precio','terreno_m2','construccion_m2');
+        $crud->field_type('idnave_industrial', 'hidden', $this->getRandomCode());
+        $crud->set_language("spanish");
+        $crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
+        $output = $crud->render();
+        $this->_example_output($output);
+}
+else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+}
+
+function rancho(){
+     if($this->session->userdata('logged_in'))
+   {
+    $crud = new grocery_CRUD();
+        $crud->set_table('rancho');
+        $crud->set_subject('Ranchos');
+        $crud->columns('precio','terreno_m2','construccion_m2');
+        $crud->field_type('idrancho', 'hidden', $this->getRandomCode());
+        $crud->set_language("spanish");
+        //$crud->add_action('Equippos', base_url('assets/grocery_crud/themes/flexigrid/css/images/add_image.png'), 'admin/imageUpload');
+        $output = $crud->render();
+        $this->_example_output($output);
+}
+else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+}
 
 function imageUpload($primary_key)
 {
-      
-    
-   
+    //$crud = new grocery_CRUD();
+    //$crud->field_type('idrancho', 'hidden', $this->getRandomCode());
     $image_crud = new image_CRUD();
- 
-    $image_crud->set_table('example_1');
-    
+    $image_crud->set_table('images');
     //If your table have by default the "id" field name as a primary key this line is not required
     $image_crud->set_primary_key_field('id');
     $image_crud->set_url_field('url');
     $image_crud->set_image_path('assets/uploads');
-    $image_crud->set_relation_field('idCasa');
+    $image_crud->set_title_field('title');
+    //$image_crud->set_ordering_field('priority');
+    $image_crud->set_relation_field('idcasa');
     $output = $image_crud->render();
  
     $this->_example_output($output);
 }
+
+
+function getRandomCode(){
+    $an = "0123456789";
+    $su = strlen($an) - 1;
+    return substr($an, rand(0, $su), 1) .
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1).
+            substr($an, rand(0, $su), 1);
+    }
+
+function logout()
+ {
+   $this->session->unset_userdata('logged_in');
+   session_destroy();
+   redirect('login', 'refresh');
+ }
 
 }
 ?>
--- administrador/application/views/example.php
+++ administrador/application/views/example.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
-<html lang="en" class="no-js">
+<html>
 <head>
 	<meta charset="utf-8" />
 
-		<!--<link rel="shortcut icon" href="../favicon.ico">-->
-		<link rel="stylesheet" type="text/css" href=<?php echo base_url() . 'assets/css/normalize.css'; ?> />
-		<link rel="stylesheet" type="text/css" href=<?php echo base_url() . 'assets/css/demo.css'; ?> />
-		<link rel="stylesheet" type="text/css" href=<?php echo base_url() . 'assets/css/component.css'; ?> />
-		
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascrpt">
+    $("#save-and-go-back-button").keyup(function(){
+       alert("there is a keyup");
+    });
+</script>
 
 <?php 
 foreach($css_files as $file): ?>
 	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
 <?php endforeach; ?>
-<script src=<?php echo base_url() . 'assets/js/modernizr.custom.js'; ?>></script>
 <?php foreach($js_files as $file): ?>
 	<script src="<?php echo $file; ?>"></script>
 <?php endforeach; ?>
+
 <style type='text/css'>
 body
 {
@@ -33,80 +34,29 @@ a:hover
 	text-decoration: underline;
 }
 </style>
-		
-		
+
 
 </head>
 <body>
 
 
-	<div class="container">
-			<ul id="gn-menu" class="gn-menu-main">
-				<li class="gn-trigger">
-					<a class="gn-icon gn-icon-menu"><span>Menu</span></a>
-					<nav class="gn-menu-wrapper">
-						<div class="gn-scroller">
-							<ul class="gn-menu">
-								<li class="gn-search-item">
-									<input placeholder="Search" type="search" class="gn-search">
-									<a class="gn-icon gn-icon-search"><span>Search</span></a>
-								</li>
-								<li>
-									<a class="gn-icon gn-icon-download">Downloads</a>
-									<ul class="gn-submenu">
-										<li><a class="gn-icon gn-icon-illustrator">Vector Illustrations</a></li>
-										<li><a class="gn-icon gn-icon-photoshop">Photoshop files</a></li>
-									</ul>
-								</li>
-								<li><a class="gn-icon gn-icon-cog">Settings</a></li>
-								<li><a class="gn-icon gn-icon-help">Help</a></li>
-								<li>
-									<a class="gn-icon gn-icon-archive">Archives</a>
-									<ul class="gn-submenu">
-										<li><a class="gn-icon gn-icon-article">Articles</a></li>
-										<li><a class="gn-icon gn-icon-pictures">Images</a></li>
-										<li><a class="gn-icon gn-icon-videos">Videos</a></li>
-									</ul>
-								</li>
-							</ul>
-						</div><!-- /gn-scroller -->
-					</nav>
-				</li>
-				<li><a href="http://tympanus.net/codrops">Codrops</a></li>
-				<li><a class="codrops-icon codrops-icon-prev" href="http://tympanus.net/Development/HeaderEffects/"><span>Previous Demo</span></a></li>
-				<li><a class="codrops-icon codrops-icon-drop" href="http://tympanus.net/codrops/?p=16030"><span>Back to the Codrops Article</span></a></li>
-			</ul>
-<!--
-			<div >
-				<?php echo $output; ?>
-    		</div>
-    		-->
-			
-			<header>
-				<?php echo $output; ?>
-			</header> 
+	 <div>
+		<a href='<?php echo site_url('admin/')?>'>Casas</a> |
+		<a href='<?php echo site_url('admin/terrenos')?>'>Terrenos</a> |
+		<a href='<?php echo site_url('admin/departamentos')?>'>Departamentos</a> |
+		<a href='<?php echo site_url('admin/bodegas')?>'>Bodegas</a> | 
+		<a href='<?php echo site_url('admin/oficinas')?>'>Oficinas</a> |		 
+		<a href='<?php echo site_url('admin/locales')?>'>Locales</a> | 
+		<a href='<?php echo site_url('admin/nave_industrial')?>'>Naves Industriales</a> | 
+		<a href='<?php echo site_url('admin/rancho')?>'>Ranchos</a> | 
+        <a href='<?php echo site_url('home/logout')?>'>Salir</a> |
+				<!--<a href='<?php echo site_url('examples/film_management_twitter_bootstrap')?>'>Twitter Bootstrap Theme [BETA]</a> | 
+		<a href='<?php echo site_url('examples/multigrids')?>'>Multigrid [BETA]</a> -->
 		
-		</div><!-- /container -->
-		<script src=<?php echo base_url() . 'assets/js/classie.js'; ?>></script>
-		<script src=<?php echo base_url() . 'assets/js/gnmenu.js'; ?>></script>
-		<script>
-			new gnMenu( document.getElementById( 'gn-menu' ) );
-		</script>
-
-
-<!--
-	<div>
-		<a href='<?php echo site_url('images_examples/example1')?>'>Example 1 - Simple</a> |
-		<a href='<?php echo site_url('images_examples/example2')?>'>Example 2 - Ordering</a> |
-		<a href='<?php echo site_url('images_examples/example3/22')?>'>Example 3 - With group id</a> |
-		<a href='<?php echo site_url('images_examples/example4')?>'>Example 4 - Images with title</a> | 
-		<a href='<?php echo site_url('images_examples/simple_photo_gallery')?>'>Simple Photo Gallery</a>
-	</div>
+	</div> 
 	<div style='height:20px;'></div>  
--->
-   
-
-
-
+    <div>
+		<?php echo $output; ?>
+    </div>
 </body>
 </html>
